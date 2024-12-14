7082532d6e975c48df9b7b625b762e71c0eb9208
cody123-hyb <s20014@ypicallmc.edu.hk>
2024-12-14 02:17:14 +0000

    update

---
+++ abc.py/def.py
@@ -0,0 +1,36 @@
+while True:
+    print("""
+    Personal Transaction
+  1.Add Transaction
+  2.Edit Transaction
+  3.Delete Transaction
+  4.View Summary
+  5.Save and Exit""")
+    
+    if choice=="1":
+        input("enter the date(YYYY-MM-DD):")
+        input("enter the category(e.g Food Transport):")
+        input("enter the amount:")
+        input("enter the type(expense?Income):")
+    
+    elif choice=="2":
+        input("enter the date(YYYY-MM-DD):")
+        input("enter the category(e.g Food Transport):")
+        input("enter the amount:")
+        input("enter the type(expense?Income):")
+
+    elif choice=="3":
+        input("enter the date(YYYY-MM-DD):")
+        input("enter the category(e.g Food Transport):")
+        input("enter the amount:")
+        input("enter the type(expense?Income):")
+
+    elif choice=="4":
+        input("enter the date(YYYY-MM-DD):")
+        input("enter the category(e.g Food Transport):")
+        input("enter the amount:")
+        input("enter the type(expense?Income):")    
+
+    elif choice=="5":
+        print("Save and Exit")    
+        break

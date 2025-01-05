a1af5a2e62c20d6af3323300832f539f142f8868
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-05 12:50:27 +0800

    update

--- practise/2.py
+++ practise/2.py
@@ -1,26 +1,25 @@
 import pandas as pd
-
+data_file = "expenses.csv"
 while True:
-    print("1. 新增交易")
-    print("2. 編輯交易")
-    print("3. 刪除交易")
+    print("1. 新增交易") 
+    print("2. 編輯交易") 
+    print("3. 刪除交易") 
     print("4. 查看摘要")
     print("5. 儲存並退出")
-    input('Enter your choice: ')
 
-if choice == '1':
-    add_transaction()
-    date = input("Enter the date: ")
-    category = input("Enter the category: ")
-    amount = input("Enter the amount: ")
-    type = input("Enter the type: ")
-    Data = (date, category, amount, type)
-    print(Data)
-    df = pd.read_csv('expenses.csv')
-
+    a = input("Enter your choice: ")
+    print(a)
+    if a == '1': 
+        date = input("Enter the date: ") 
+        category = input("Enter the category: ") 
+        amount = input("Enter the amount: ") 
+        type_ = input("Enter the type: ") 
+        Data = (date, category, amount, type_) 
+        print(Data)
+        df = pd.read_csv('expenses.csv')
 
     if not os.path.exists('expenses.csv'):
         df = pd.DataFrame(columns=columns)
         print("無現有檔案，建立新資料表")
         print(df)
-    print("Data saved to expenses.csv.")
+        print("Data saved to expenses.csv.")

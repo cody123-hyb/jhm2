f242c8749d47a99544cd929ed58e1a5fdb716a54
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-05 13:33:21 +0800

    update

--- practise/2.py
+++ practise/2.py
@@ -28,7 +28,7 @@ while True:
     
     
     #編輯交易
-    elif a == '2' :
+    elif a == '2':
         index = int(input("輸入要編輯的交易索引: "))
         print(df.loc[index]) 
         new_date = input("輸入新日期 (Enter 保留原值): ") or df.loc[index, 'Date']
@@ -37,14 +37,14 @@ while True:
     
     
     #刪除交易
-    elif a == '3' :
+    elif a == '3':
         index = int(input("輸入要刪除的交易索引: "))
         df = df.drop(index).reset_index(drop=True)
         print("交易刪除成功！")
 
     
     #查看摘要
-    elif a == '4' :
+    elif a == '4':
         total_expenses = df[df['Type'] == '支出']['Amount'].sum()
         total_income = df[df['Type'] == '收入']['Amount'].sum()
         savings = total_income - total_expenses
@@ -54,7 +54,7 @@ while True:
         if total_expenses == 0 and total_income == 0: print("無相關資料")
 
     #儲存並退出
-    elif a == '5' :
+    elif a == '5':
         df.to_csv('expenses.csv', index=False)
         print("資料已儲存！")
         break

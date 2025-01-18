4285fa046abcf0de275fea2d81ffe62a86b1aafa
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-18 22:16:37 +0800

    update

--- jhm2homework/加法口訣表.py
+++ jhm2homework/加法口訣表.py
@@ -1,9 +1,9 @@
 # 獲取用戶輸入的口訣表大小
-size = int(input("請輸入口訣表大小 (小於 10 的正整數): "))
+Sizea = int(input("Input Addition Table Size smaller 10: "))
 
 # 列印加法口訣表
-print(f"\n{size} 以內的加法口訣表:")
-for i in range(1, size + 1):
+print("Addition Table")
+for i in range(1, size + 1 =()):
     for j in range(1, size + 1):
         result = i + j
         # 當兩個數的和為個位數時，補充兩個空格；當兩個數的和為兩位數時，補充一個空格

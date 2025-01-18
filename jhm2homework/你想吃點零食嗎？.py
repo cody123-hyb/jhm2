471f999d2a5536f0beee987eb8d64a44d6cad691
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-18 20:54:09 +0800

    update

--- jhm2homework/你想吃點零食嗎？.py
+++ jhm2homework/你想吃點零食嗎？.py
@@ -4,7 +4,10 @@ second_question = input("Enter your choice (ice-cream / cookies / candies ): ")
 a = int(first_question)
 b = int(second_question)
 
-if a == "yes" :
+if a == "no" : 
+    print("Good! Let's play games instead.")             
+
+elif a == "yes" :
     input("Enter your choice (ice-cream / cookies / candies ): ")
     
     if b == "ice cream" :
@@ -16,5 +19,3 @@ if a == "yes" :
     if b == "candies" :
         print("Don't eat too much.")
 
-elif a == "no" : 
-    print("Good! Let's play games instead.")             

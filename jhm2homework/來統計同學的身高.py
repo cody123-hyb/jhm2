e5f16203e2ccb908f372dc0a1ad9409d07d4d0ce
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-19 16:30:34 +0800

    update

--- jhm2homework/來統計同學的身高.py
+++ jhm2homework/來統計同學的身高.py
@@ -1,14 +1,29 @@
-if numbera and numberb and numberc and numberd < 0 :
+numbera = input ("Input the height of a student 1 in cm: ")
+if numbera < 0 :
     print("Height must be positive.")
 
-elif numbera and numberb and numberc and numberd > 200 :
+elif numbera > 200 :
     print("Height is greater than 200cm.")
     
-
-numbera = input ("Input the height of a student 1 in cm: ")
 numberb = input ("Input the height of a student 2 in cm: ")
+if numberb < 0 :
+    print("Height must be positive.")
+
+elif numberb > 200 :
+    print("Height is greater than 200cm.")
+    
 numberc = input ("Input the height of a student 3 in cm: ")
+if numberc < 0 :
+    print("Height must be positive.")
+
+elif numberc > 200 :
+    print("Height is greater than 200cm.")
 numberd = input ("Input the height of a student 4 in cm: ")
+if numberd < 0 :
+    print("Height must be positive.")
+
+elif numberd > 200 :
+    print("Height is greater than 200cm.")
 
 
 

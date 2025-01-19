c7daa58ee8107fbc5887ba033bd56a3c90ab2137
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-19 17:15:34 +0800

    update

--- jhm2homework/來統計同學的身高.py
+++ jhm2homework/來統計同學的身高.py
@@ -1,35 +1,34 @@
-times = 1
+time = 0
 
-
-while times < 4 :
-    a = int(input("Input the height of a student 1 in cm: "))
-    if a < 0: 
+while time < 4 :
+ a = int(input("Input the height of a student 1 in cm: "))
+ if a < 0: 
         print("Height must be positive.") 
-    elif a > 200: 
+ elif a > 200: 
         print("Height is greater than 200cm.") 
-    else:
-
-        b = int(input("Input the height of a student 2 in cm: "))
-        if b < 0: 
+ else:
+    time +=1
+    b = int(input("Input the height of a student 2 in cm: "))
+    if b < 0: 
             print("Height must be positive.") 
-        elif b > 200: 
+    elif b > 200: 
             print("Height is greater than 200cm.") 
-        else:
-
-            c = int(input("Input the height of a student 3 in cm: "))
-            if c < 0: 
+    else:
+        time +=1
+        c = int(input("Input the height of a student 3 in cm: "))
+        if c < 0: 
                 print("Height must be positive.") 
-            elif c > 200: 
+        elif c > 200: 
                 print("Height is greater than 200cm.") 
-            else:
-        
-                d = int(input("Input the height of a student 4 in cm: "))
-                if d < 0: 
+        else:
+            time +=1
+            d = int(input("Input the height of a student 4 in cm: "))
+            if d < 0: 
                     print("Height must be positive.") 
-                elif d > 200: 
-                    print("Height is greater than 200cm.")           
+            elif d > 200: 
+                    print("Height is greater than 200cm.")  
+                    time +=1            
 
-print ("End of Input")
 
 
 

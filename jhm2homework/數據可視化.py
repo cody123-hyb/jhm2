d4db1eee30d35ec6b85e0becbeb19ce2c08e937a
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-20 21:52:53 +0800

    update

---
+++ jhm2homework/數據可視化.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as plt
+file_name = 'csv文件1'
+if os.path.exists(file_name):
+    df = pd.read_csv(file_name)
+
+x = [Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Set,Oct,Nov,Dec]
+y = [0,500,1000,1500,2000]
+plt.plot(x,y,label='Data',color='b',marker='o')

0e5089f7208d029b0734556e255e56240b1f561a
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 21:48:07 +0800

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -9,9 +9,13 @@ bars = ax.bar(bar_labels, bar_values, color="salmon")
 # 为条形添加数字标签, padding是标签与条形的距离
 ax.bar_label(bars, padding=3)
 
+data = { 'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+         'Sales': [380,400,660,800,900,1200,1600,2200,1500,1100,600,250] }
+
 # 设置坐标轴标签
 ax.set_xlabel("X (unit)", fontweight="bold")
 ax.set_ylabel("Y (unit)", fontweight="bold")
+
 
 # 添加图例
 _ = ax.legend()

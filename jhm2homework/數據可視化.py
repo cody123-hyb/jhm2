b2c384678e059b7dcff6cebbb3d830f9129c75ef
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 14:08:46 +0000

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -1,19 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-fig, ax = plt.subplots(constrained_layout=True)
 
-# bar_labels是一个字符串列表，bar_values是一个与bar_labels等长的数字列表
-# 更多参数：https://www.wolai.com/matplotlib/2mypc3HyBTyLR8TQKmxVku
-bars = ax.bar(bar_labels, bar_values, color="salmon")
+x = [Jan , Feb , Mar , Apr , May , Jun , Jul , Aug , Sep , Oct , Nov , Dec]
+y = [380 , 400 , 660 , 800 ,  900 , 1200 , 1600 , 2200 , 1500 , 1100 , 600 , 250]
 
-# 为条形添加数字标签, padding是标签与条形的距离
-ax.bar_label(bars, padding=3)
-
-# 设置坐标轴标签
-ax.set_xlabel("X (unit)", fontweight="bold")
-ax.set_ylabel("Y (unit)", fontweight="bold")
-
-# 添加图例
-_ = ax.legend()
-
+plt.bar(x,y)
 plt.show()

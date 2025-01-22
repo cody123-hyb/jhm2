15a69234ea71629969c028b3060b0772bd1a1b50
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 14:48:45 +0000

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -11,10 +11,8 @@ data = {
 df = pd.DataFrame(data)
 df.to_csv('sales.csv', index=False)
 
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-y = [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
 colora = 'green'
-plt.bar(x,y , color = colora , width=0.5)
+plt.bar(Month , Sales , color = colora , width=0.5)
 plt.title("Bar Chart of ice-cream Sales")
 plt.xlabel("Month")
 plt.ylabel("Sales(in thousand dollars)")

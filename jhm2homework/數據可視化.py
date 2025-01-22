08b19fc9f99166912224bf84b57dd6e7aa93eba6
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 14:55:11 +0000

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = {
-'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+'Montha': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 'Sales': [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
 }
 df = pd.DataFrame(data)
 df.to_csv('sales.csv', index=False)
+df = pd.read_csv('sales.csv')
 
 
-a = df['Month']
+a = df['Montha']
 b = df['Sales']
 colora = 'green'
 plt.bar(a,b,color = colora,width=0.5)

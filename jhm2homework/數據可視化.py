c09aac7bf7ebc325d7b9808373683d2de7a08760
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 14:44:30 +0000

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -2,8 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+import pandas as pd
 
-df = pd.read_csv('sales.csv')
+data = {
+'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+'Sales': [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
+}
+df = pd.DataFrame(data)
+df.to_csv('sales.csv', index=False)
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 y = [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]

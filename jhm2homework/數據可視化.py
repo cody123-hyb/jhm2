c6774cb025b3191ad26d9894bb6658bc5465375a
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 14:10:20 +0000

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-x = [Jan , Feb , Mar , Apr , May , Jun , Jul , Aug , Sep , Oct , Nov , Dec]
+a = Jan = jls_extract_def()
+x = [a , Feb , Mar , Apr , May , Jun , Jul , Aug , Sep , Oct , Nov , Dec]
 y = [380 , 400 , 660 , 800 ,  900 , 1200 , 1600 , 2200 , 1500 , 1100 , 600 , 250]
 
 plt.bar(x,y)

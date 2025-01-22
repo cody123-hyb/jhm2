9577a81c9792f38c99f2784b209df3f2a1ca0194
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 14:38:36 +0000

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -1,10 +1,14 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+df = pd.read_csv('sales.csv')
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 y = [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
 colora = 'green'
-plt.bar(x,y , color = colora)
+plt.bar(x,y , color = colora , width=0.5)
 plt.title("Bar Chart of ice-cream Sales")
 plt.xlabel("Month")
 plt.ylabel("Sales(in thousand dollars)")

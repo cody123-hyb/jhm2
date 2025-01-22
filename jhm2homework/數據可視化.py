64e5fb4b75c2849ac4c4c2cba4dc283004c2c8f8
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-22 14:34:50 +0000

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 y = [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
-colora = ['green']
+colora = 'green'
 plt.bar(x,y , color = colora)
 plt.title("Bar Chart of ice-cream Sales")
 plt.xlabel("Month")

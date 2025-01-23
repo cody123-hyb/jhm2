e36aa9e4658c8e390661cb601a64506dfde40393
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-23 14:11:52 +0000

    修正加法表輸出格式，調整內部迴圈的縮排並更改輸出順序

--- jhm2homework/加法口訣表.py
+++ jhm2homework/加法口訣表.py
@@ -3,9 +3,9 @@ a = Sizea
 print("Addition Table")
 
 for i in range (1 , a + 1):
-        for j in (1 , a + 1):
+    for j in (1 , a + 1):
         cs = i + j
-        print(f'{i} + {j} = {cs}\t', end =' ')
-    print()
+        print(f'{j} + {i} = {cs}\t', end =' ')
+    print( )
 
   

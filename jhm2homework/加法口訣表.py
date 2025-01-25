9a251145148a58c3407614fc573bb521c01a251e
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-25 11:23:18 +0800

    update

--- jhm2homework/加法口訣表.py
+++ jhm2homework/加法口訣表.py
@@ -2,13 +2,17 @@ Sizea = int(input("Input Addition Table Size smaller 10: "))
 a = Sizea
 print("Addition Table")
 
+print("------------------------------------------------------------")
+
+
 for i in range (1 , a + 1):
     for j in (1 , a + 1):
-        cs = j + i
+        cs = i + j
+
         if cs < 10:
-            print(f'{} + {i} = {cs}', end ='  ')
+            print(f'{i} + {j} = {cs}', end ='  ')
         else: 
-            print(f'{} + {i} = {cs}', end =' ')   
+            print(f'{i} + {j} = {cs}', end =' ')   
     print( )
 
-  
+print("------------------------------------------------------------")

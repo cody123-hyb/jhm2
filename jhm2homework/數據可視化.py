6ee57031b9cda45533f3b4485945a55c438b8dd4
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-01-25 10:08:34 +0800

    update

--- jhm2homework/數據可視化.py
+++ jhm2homework/數據可視化.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = {
-'Montha': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-'Salesa': [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
+'Month_a': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+'Sales_a': [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
 }
 df = pd.DataFrame(data)
 df.to_csv('sales.csv', index=False)
 df = pd.read_csv('sales.csv')
 
 
-a = df['Montha']
-b = df['Salesa']
+a = df['Month_a']
+b = df['Sales_a']
 
 plt.bar(a,b,color = 'green',width=0.5)
 plt.title("Bar Chart of ice-cream Sales")

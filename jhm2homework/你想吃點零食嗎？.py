a6c100283ddbe4f8aef6f1b9166ca28fa1eb9a35
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-02-11 19:23:23 +0800

    Update 你想吃點零食嗎？.py

--- jhm2homework/你想吃點零食嗎？.py
+++ jhm2homework/你想吃點零食嗎？.py
@@ -1,9 +1,9 @@
-first_question = input("Do you want some snacks? (yes/no): ").lower()
+first_question = input("Do you want some snacks? (yes/no): ")
 if first_question == "no":
     print("Good! Let's play games instead.")
 
 elif first_question == "yes":
-    second_question = input("Enter your choice (ice-cream / cookies / candies): ").lower()
+    second_question = input("Enter your choice (ice-cream / cookies / candies): ")
     
     if second_question == "ice-cream":
         print("Remember to wash your hands.")

328ed1719a79161e2838691383befa11b36169fe
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-02-15 12:11:30 +0800

    update

--- jhm2homework/加法口訣表.py
+++ jhm2homework/加法口訣表.py
@@ -7,6 +7,6 @@ for b in range(size + 1):
         a = [f"{b} + {c} = { b + c }" for j in range(size + 1)]
         formatted_a = [f"{entry:<11}" for entry in a]
 
-    print("".join(formatted_a))
+    
 
 print("------------------------------------------------------------")

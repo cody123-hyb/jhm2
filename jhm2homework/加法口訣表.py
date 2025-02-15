f808099a43f3eb24cc51e476a38b10def3e18346
cody123-hyb <s20014@ypicallmc.edu.hk>
2025-02-15 12:55:46 +0800

    update

--- jhm2homework/加法口訣表.py
+++ jhm2homework/加法口訣表.py
@@ -5,10 +5,10 @@ print("------------------------------------------------------------")
 for b in range(size + 1):
     for c in range(size + 1):
         a = [f"{b} + {c} = {b + c}{'  ' if b + c < 10 else ' '}" for j in range(1)]
-        formatted_a = [f"{entry:<11}" for entry in a]
+    formatted_a = [f"{entry:<11}" for entry in a]
         
         
-        print("".join(formatted_a))
+    print("".join(formatted_a))
     
 print("------------------------------------------------------------")
 
